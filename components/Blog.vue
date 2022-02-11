fabd3fbf6c20ad38a9b8f00e408777aab9b88f83
Amri <amrikarisma@yahoo.com>
2022-02-12 05:54:04 +0700

    get list blog

---
+++ components/Blog.vue
@@ -0,0 +1,25 @@
+<script setup>
+const { data } = await useAsyncData('count', () => $fetch('https://api.nuxtjs.dev/mountains'))
+</script>
+
+<template>
+    <section class="page-section" id="blog">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-4" v-for="(post, index) in data" :key="index">
+                    <NuxtLink :to="post.slug">
+                        <div class="card">
+                            <div v-if="post.image">
+                                <img class="img-fluid" v-bind:src="post.image" />
+                            </div>
+                            <div class="card-body">
+                                <h2>{{ post.title }}</h2>
+                                <p>{{post.description}}</p>
+                            </div>
+                        </div>
+                    </NuxtLink>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ pages/blog.vue
@@ -0,0 +1,26 @@
+<template>
+    <Blog />
+</template>
+<script>
+export default {
+
+    data() {
+      return {
+        title: 'Blog - Nuxt CMS Blog'
+      }
+    },
+    head() {
+      return {
+          title: 'Blog - Nuxt CMS Blog',
+          meta: [
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Nuxt CMS Blog cooming soon'
+            }
+        ]
+      }
+    }
+}
+
+</script>
