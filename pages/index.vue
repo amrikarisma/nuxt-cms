1a547c9a0af2172c21845066e601d1da72e42684
Amri <amrikarisma@yahoo.com>
2022-02-12 02:13:52 +0700

    add template

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <Header />
+    <Portofolio />
+    <About />
+    <Contact />
+</template>
+<script>
+export default {
+    asyncData() {
+        return {
+            rendering: process.server ? 'server' : 'client'
+        }
+    },
+
+}
+
+</script>
